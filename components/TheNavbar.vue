223be2051c9d7694bd5626fc0e7181772fcc3171
Daniel Naschberger <daniel@naschi.at>
2021-05-26 19:48:03 +0200

    chore(TheNavbar): init navbar

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>TheNavbar.vue</div>
+  <div class="container mx-auto bg-blue-400 p-5">
+    <nav class="flex justify-between">
+      <div>
+        <NuxtLink to="/">Logo</NuxtLink>
+      </div>
+      <ul class="flex flex-row">
+        <li class="pr-5"><NuxtLink to="/tandem">Tandemflüge</NuxtLink></li>
+        <li class="pr-5"><NuxtLink to="/ausbildung">Ausbildung</NuxtLink></li>
+        <li class="pr-5"><NuxtLink to="/team">Team</NuxtLink></li>
+        <li><NuxtLink to="/kontakt">Kontakt</NuxtLink></li>
+      </ul>
+    </nav>
+  </div>
 </template>
