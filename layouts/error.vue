c836e601b3f2060884ed2d87add08f807a402d7f
Daniel Naschberger <daniel@naschi.at>
2021-05-26 19:47:14 +0200

    chore(error): add error page

---
+++ layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">404 Page not found</h1>
+    <h1 v-else>Es ist ein Fehler aufgetretten</h1>
+    <NuxtLink to="/">zurück zur Startseite</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+  props: ['error'],
+}
+</script>
