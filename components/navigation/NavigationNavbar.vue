e61622c96149ce2afae34331ef1aea8ada3edfb8
Daniel Naschberger <daniel@naschi.at>
2021-05-29 13:44:57 +0200

    fix(NavigationNavbar): Teamlink

--- components/navigation/NavigationNavbar.vue
+++ components/navigation/NavigationNavbar.vue
@@ -2,7 +2,7 @@
   <div class="flex">
     <navigation-navbar-link name="Tandemfliegen" to="/tandemfliegen" />
     <navigation-navbar-link name="Ausbildung" to="/ausbildung" />
-    <navigation-navbar-link name="Team" to="/ausbildung" />
+    <navigation-navbar-link name="Team" to="/team" />
   </div>
 </template>
 
