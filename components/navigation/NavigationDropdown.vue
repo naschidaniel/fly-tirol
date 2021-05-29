c11bdc35bf17583162a6ff023ec680596d87aa05
Daniel Naschberger <daniel@naschi.at>
2021-05-29 18:14:04 +0200

    chore(Navigation): update entries

--- components/navigation/NavigationDropdown.vue
+++ components/navigation/NavigationDropdown.vue
@@ -8,7 +8,14 @@
       <navigation-dropdown-link name="Home" to="/" />
       <navigation-dropdown-link name="Tandemfliegen" to="/tandemfliegen" />
       <navigation-dropdown-link name="Ausbildung" to="/ausbildung" />
+      <navigation-dropdown-link name="Fortbildung" to="/fortbildung" />
+      <navigation-dropdown-link name="Reisen" to="/reisen" />
+      <navigation-dropdown-link
+        name="Sicherheitstrainings"
+        to="/sicherheitstrainings"
+      />
       <navigation-dropdown-link name="Team" to="/team" />
+      <navigation-dropdown-link name="Kontakt" to="/kontakt" />
     </div>
   </div>
 </template>
