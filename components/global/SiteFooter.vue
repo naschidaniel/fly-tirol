4aa4a40329e632f908a76ea6f42eebeb2e427d69
Daniel Naschberger <daniel@naschi.at>
2021-05-29 16:57:43 +0200

    chore(Impressum): show Sourcode and Infomations

--- components/global/SiteFooter.vue
+++ components/global/SiteFooter.vue
@@ -1,22 +1,11 @@
 <template>
   <div class="text-center">
-    <small class="text-sm text-blue-500 mr-6"> &#169; feel-Free.tirol </small>
+    <small class="text-sm text-gray-500 mr-6"> &#169; feel-Free.tirol </small>
     <nuxt-link to="/dsvgo">
-      <small class="text-sm text-blue-500 mr-6"> DSVGO </small>
+      <small class="text-sm text-gray-500 mr-6">DSVGO</small>
     </nuxt-link>
     <nuxt-link to="/impressum">
-      <small class="text-sm text-blue-500 mr-6"> Impressum </small>
+      <small class="text-sm text-gray-500 mr-6">Impressum</small>
     </nuxt-link>
-    <small class="text-sm text-blue-500 mr-6">
-      Version: {{ NUXT_ENV_CURRENT_GIT_SHA }}
-    </small>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return { NUXT_ENV_CURRENT_GIT_SHA: process.env.NUXT_ENV_CURRENT_GIT_SHA }
-  },
-}
-</script>
---
+++ pages/impressum.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <nuxt-content :document="impressum" />
+    <h2>Sourcecode der Webseite</h2>
+    <div>
+      Github Repository:
+      <a href="https://github.com/naschidaniel/feelfree" target="_blank"
+        >https://github.com/naschidaniel/feelfree</a
+      >
+    </div>
+    <div>Version: {{ NUXT_ENV_CURRENT_GIT_SHA }}</div>
+    <div>
+      Buildtime:
+      {{ NUXT_ENV_CURRENT_TIMESTAMP | formatDateTime }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const impressum = await $content('impressum').fetch()
+    return { impressum }
+  },
+  data() {
+    return {
+      NUXT_ENV_CURRENT_GIT_SHA: process.env.NUXT_ENV_CURRENT_GIT_SHA,
+      NUXT_ENV_CURRENT_TIMESTAMP: process.env.NUXT_ENV_CURRENT_TIMESTAMP,
+    }
+  },
+}
+</script>
