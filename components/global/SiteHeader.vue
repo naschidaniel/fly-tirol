99b0dada053ba09c2fae545e4205b881a45b3811
Daniel Naschberger <daniel@naschi.at>
2021-05-29 16:56:38 +0200

    chore(SiteHeader): fix styling for small sizes

--- components/global/SiteHeader.vue
+++ components/global/SiteHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <nav class="bg-gray-100">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl sm:px-2 lg:px-8 4xl:px-16">
       <div class="flex items-center justify-between h-10">
-        <div class="flex items-center border-3">
+        <div class="flex items-center">
           <div class="flex-shrink-0">
             <nuxt-link
               :to="{ name: 'index' }"
@@ -14,7 +14,7 @@
             </nuxt-link>
           </div>
           <div class="hidden sm:block sm:ml-6">
-            <navigation-navbar />
+            <navigation-navbar class="ml-12" />
           </div>
         </div>
         <div class="ml-auto sm:ml-6 sm:block">
