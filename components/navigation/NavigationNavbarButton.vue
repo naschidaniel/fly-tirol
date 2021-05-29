df9c2a21f0b205825f6ce162a22d4d6be411d8c1
Daniel Naschberger <daniel@naschi.at>
2021-05-29 13:08:09 +0200

    chore(navigationNavbarButton): use hericonicons

--- components/navigation/NavigationNavbarButton.vue
+++ components/navigation/NavigationNavbarButton.vue
@@ -19,7 +19,36 @@
       "
       @click="toggleDropdown()"
     >
-      <i class="bi" :class="isOpen ? 'bi-x-lg' : 'bi-list'"></i>
+      <svg
+        v-if="isOpen"
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M6 18L18 6M6 6l12 12"
+        />
+      </svg>
+      <svg
+        v-else
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M4 6h16M4 10h16M4 14h16M4 18h16"
+        />
+      </svg>
     </button>
   </div>
 </template>
