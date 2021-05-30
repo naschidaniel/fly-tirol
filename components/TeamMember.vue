dd142a61f6e459eee9abd8d2fcc7aa7171735426
Daniel Naschberger <daniel@naschi.at>
2021-05-30 21:48:51 +0200

    chore(TeamMember): prettier

--- components/TeamMember.vue
+++ components/TeamMember.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="rounded-xl p-4 bg-gray-50 m-8 border-2 border-gray-300 shadow-sm">
-    <h3 class="text-cyan-600">
+  <div
+    class="rounded-xl p-4 bg-gray-50 mb-4 border-2 border-gray-300 shadow-sm"
+  >
+    <h3 class="text-cyan-600 mt-2">
       {{ name }}
     </h3>
-    <div class="md:flex">
+    <div class="md:flex mt-2">
       <img
         class="mb-2 object-none rounded-lg mx-auto md:m-2"
         src="https://www.x-dreamfly.ch/wp-content/uploads/2017/06/Sebastian_Kahn_Pilot_X-dreamfly.jpg"
@@ -13,10 +15,10 @@
         :title="name"
       />
       <div class="md:flex-1">
-        <p class="text-gray-600">
+        <p class="font-mono text-gray-600 text-lg md:text-xl">
           {{ position }}
         </p>
-        <p>
+        <p class="mt-2 text-base md:text-lg">
           {{ description }}
         </p>
       </div>
