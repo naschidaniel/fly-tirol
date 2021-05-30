09f29d7ccb343531f275cfeddaafbdba4a7bcb10
Daniel Naschberger <daniel@naschi.at>
2021-05-30 22:36:02 +0200

    chore(courses): list courses

---
+++ components/CourseCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="rounded-xl p-4 bg-gray-50 border-2 border-gray-300 shadow-sm">
+    <h3 class="text-cyan-600">
+      {{ course.title }}
+    </h3>
+    <p class="text-gray-600">
+      {{ course.description }}
+    </p>
+    <p>
+      {{ course.information }}
+    </p>
+    <div class="flex content-start">
+      <div class="flex-1">
+        {{ course.price }}
+      </div>
+      <div class="text-right">
+        <feel-free-button :to="course.path" name="Details" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FeelFreeButton from './FeelFreeButton.vue'
+export default {
+  components: { FeelFreeButton },
+  props: ['course'],
+}
+</script>
---
+++ components/CoursesList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div v-for="course in courses" :key="course.slug">
+      <course-card class="m-2" :course="course" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CourseCard from './CourseCard.vue'
+export default {
+  components: { CourseCard },
+  props: ['courses'],
+}
+</script>
