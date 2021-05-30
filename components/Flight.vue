1c331fe39c3199f515d25c040db696131a4a76ea
Daniel Naschberger <daniel@naschi.at>
2021-05-30 22:36:45 +0200

    fix(Flight): use FeelFreeButton

--- components/Flight.vue
+++ components/Flight.vue
@@ -14,39 +14,16 @@
         {{ flight.price }}
       </div>
       <div class="text-right">
-        <nuxt-link
-          to="/kontakt"
-          class="
-            border-2
-            mt-1
-            block
-            px-3
-            py-2
-            rounded-md
-            text-base
-            font-medium
-            text-gray-500
-            bg-gray-100
-            hover:text-white
-            hover:bg-gray-700
-            focus:outline-none
-            focus:text-white
-            focus:bg-gray-700
-            transition
-            duration-150
-            ease-in-out
-            cursor-pointer
-          "
-        >
-          Buchen
-        </nuxt-link>
+        <feel-free-button name="Buchen" to="/kontakt" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import FeelFreeButton from './FeelFreeButton.vue'
 export default {
+  components: { FeelFreeButton },
   props: ['flight'],
 }
 </script>
