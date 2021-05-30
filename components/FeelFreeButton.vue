140a445a2784474f6470d5bea8fd48403cdec1f6
Daniel Naschberger <daniel@naschi.at>
2021-05-30 22:35:34 +0200

    chore(FeelFreeButton): init button

---
+++ components/FeelFreeButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <nuxt-link
+    :to="to"
+    class="
+      border-2
+      mt-1
+      block
+      px-3
+      py-2
+      rounded-md
+      text-base
+      font-medium
+      text-gray-500
+      bg-gray-100
+      hover:text-white
+      hover:bg-gray-700
+      focus:outline-none
+      focus:text-white
+      focus:bg-gray-700
+      transition
+      duration-150
+      ease-in-out
+      cursor-pointer
+    "
+  >
+    {{ name }}
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: ['name', 'to'],
+}
+</script>
