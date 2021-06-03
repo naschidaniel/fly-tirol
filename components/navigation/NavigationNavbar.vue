729653558bd1b4b00be1c7368678c8591067fe7e
Daniel Naschberger <daniel@naschi.at>
2021-06-03 16:42:47 +0200

    chore(navigation): reorder menu

--- components/navigation/NavigationNavbar.vue
+++ components/navigation/NavigationNavbar.vue
@@ -3,11 +3,11 @@
     <navigation-navbar-link name="Tandemfliegen" to="/tandemfliegen" />
     <navigation-navbar-link name="Ausbildung" to="/ausbildung" />
     <navigation-navbar-link name="Fortbildung" to="/fortbildung" />
-    <navigation-navbar-link name="Reisen" to="/reisen" />
     <navigation-navbar-link
       name="Sicherheitstrainings"
       to="/sicherheitstrainings"
     />
+    <navigation-navbar-link name="Reisen" to="/reisen" />
     <navigation-navbar-link name="Team" to="/team" />
     <navigation-navbar-link name="Kontakt" to="/kontakt" />
   </div>
