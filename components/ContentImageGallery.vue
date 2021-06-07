ac075792828ceaa7d10d749b23b09445947fff53
Daniel Naschberger <daniel@naschi.at>
2021-06-07 22:34:18 +0200

    chore(ContentImageGallery): init Component

---
+++ components/ContentImageGallery.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="py-8 px-2 w-100">
+    <div class="flex flex-wrap -mx-4 mb-4">
+      <div v-for="img in imageGalery" :key="img.url" class="md:w-1/3 px-4 mb-6">
+        <responsive-image
+          class="rounded shadow-md"
+          :alt="img.alt"
+          :picture="img.url"
+          :title="img.title"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ResponsiveImage from './ResponsiveImage.vue'
+import media from '~/static/media.json'
+export default {
+  components: { ResponsiveImage },
+  props: {
+    path: { type: String, default: '/media' },
+  },
+  data() {
+    return {
+      media,
+    }
+  },
+  computed: {
+    imageGalery() {
+      return Object.values(this.media).map((img) => {
+        return img.path === this.path ? img : ''
+      })
+    },
+  },
+}
+</script>
