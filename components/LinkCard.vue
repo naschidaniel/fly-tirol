6a04172c4c13f38dc5b320760e474bc0a80032ec
Daniel Naschberger <daniel@naschi.at>
2021-06-09 21:41:20 +0200

    chore(LinkCard): init Component

---
+++ components/LinkCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="rounded-xl p-4 bg-gray-50 border-2 border-gray-300 shadow-sm">
+    <p class="text-cyan-600">
+      {{ cardTitle }}
+    </p>
+    <responsive-image height="h-100" :picture="cardImage" width="h-100" />
+    <p class="text-gray-600">
+      {{ description }}
+    </p>
+    <div class="flex content-center">
+      <div class="text-right">
+        <card-button :to="to" :name="linkName" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ResponsiveImage from './ResponsiveImage.vue'
+export default {
+  components: { ResponsiveImage },
+  props: {
+    cardTitle: { type: String, required: true },
+    cardImage: { type: String, required: true },
+    description: { type: String, default: '' },
+    linkName: { type: String, required: true },
+    to: { type: String, required: true },
+  },
+}
+</script>
