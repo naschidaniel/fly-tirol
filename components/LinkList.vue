844f8bae3f03588e8dcd022670ec18b98d566ee9
Daniel Naschberger <daniel@naschi.at>
2021-06-09 21:41:42 +0200

    chore(LinkList): add list view for linkCards

---
+++ components/LinkList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <link-card
+      :card-image="tandemflights.cardImage"
+      :card-title="tandemflights.cardTitle"
+      :description="tandemflights.description"
+      :link-name="tandemflights.linkName"
+      :to="tandemflights.to"
+    />
+    <link-card
+      :card-image="basicTrainings.cardImage"
+      :card-title="basicTrainings.cardTitle"
+      :description="basicTrainings.description"
+      :link-name="basicTrainings.linkName"
+      :to="basicTrainings.to"
+    />
+    <link-card
+      :card-image="advancedTrainings.cardImage"
+      :card-title="advancedTrainings.cardTitle"
+      :description="advancedTrainings.description"
+      :link-name="advancedTrainings.linkName"
+      :to="advancedTrainings.to"
+    />
+    <link-card
+      :card-image="saftyTrainings.cardImage"
+      :card-title="saftyTrainings.cardTitle"
+      :description="saftyTrainings.description"
+      :link-name="saftyTrainings.linkName"
+      :to="saftyTrainings.to"
+    />
+  </div>
+</template>
+
+<script>
+import LinkCard from './LinkCard.vue'
+export default {
+  components: { LinkCard },
+  props: {
+    advancedTrainings: { type: Object },
+    basicTrainings: { type: Object, required: true },
+    saftyTrainings: { type: Object, required: true },
+    tandemflights: { type: Object, required: true },
+    travel: { type: Object },
+  },
+}
+</script>
