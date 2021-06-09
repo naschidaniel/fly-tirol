022e2c37b94280d6cbf3b6664965028ba9dde3f4
Daniel Naschberger <daniel@naschi.at>
2021-06-09 21:42:36 +0200

    chore(index): add linkLists for categorys
    - advancedTrainings
    - basicTrainings
    - saftyTrainings
    - tandemflights

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,57 @@
 <template>
   <div>
     <nuxt-content :document="index" />
+    <link-list
+      :advanced-trainings="advancedTrainings"
+      :basic-trainings="basicTrainings"
+      :tandemflights="tandemflights"
+      :safty-trainings="saftyTrainings"
+    />
   </div>
 </template>
 
 <script>
+import LinkList from '~/components/LinkList.vue'
 import { generateMetatags } from '~/util/generateHeaderInformation'
 
 export default {
+  components: { LinkList },
   async asyncData({ $content }) {
     const index = await $content('index').fetch()
     return { index }
+  },
+  data() {
+    return {
+      advancedTrainings: {
+        cardTitle: 'Fortbildung',
+        cardImage: '/media/fortbildung/fortbildung.jpg',
+        description: 'Fortbildungen in der Flugschule Fly Tirol',
+        linkName: 'zu den Ausbildungen',
+        to: '/fortbildung',
+      },
+      basicTrainings: {
+        cardTitle: 'Ausbildung',
+        cardImage: '/media/ausbildung/uebungshang.jpg',
+        description: 'Schulungsbetrieb in der Flugschule Fly Tirol',
+        linkName: 'zu den Ausbildungen',
+        to: '/ausbildung',
+      },
+      saftyTrainings: {
+        cardTitle: 'Sicherheitstrainings',
+        cardImage:
+          '/media/sicherheitstrainings/sicherheitstraining-gardasee/infinit-gardasee.jpg',
+        description: 'Unsere Sicherheitstrainings mit Sebastian Kahn',
+        linkName: 'zu den Sicherheitstrainings',
+        to: '/sicherheitstrainings',
+      },
+      tandemflights: {
+        cardTitle: 'Tandemflüge',
+        cardImage: '/media/tandemfliegen/5_hohe-salve-westendorf.jpg',
+        description: 'Unsere Tandemflüge',
+        linkName: 'zu den Tandemflügen',
+        to: '/tandemfliegen',
+      },
+    }
   },
   head() {
     const metatags = generateMetatags(this.index.title, this.index.description)
