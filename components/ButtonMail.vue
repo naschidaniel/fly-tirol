cd6b12f96e737b136f822e4e7c7998af6cb1b768
Daniel Naschberger <daniel@naschi.at>
2021-06-11 21:11:05 +0200

    chore(Buttons): contact mail phone btns

---
+++ components/ButtonMail.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    class="
+      bg-green-500
+      hover:bg-green
+      text-green-darkest
+      font-bold
+      py-2
+      px-4
+      rounded
+      inline-flex
+      items-center
+    "
+  >
+    <!-- https://heroicons.dev/ - mail - MIT Licence  -->
+    <svg
+      class="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+      ></path>
+    </svg>
+    <a :href="`mailto:${mailaddress}`">
+      <span class="ml-4">{{ mailaddress }}</span>
+    </a>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    mailaddress: { type: String, required: true },
+  },
+}
+</script>
---
+++ components/ButtonPhone.vue
@@ -0,0 +1,42 @@
+<template>
+  <button
+    class="
+      bg-green-500
+      hover:bg-green
+      text-green-darkest
+      font-bold
+      py-2
+      px-4
+      rounded
+      inline-flex
+      items-center
+    "
+  >
+    <!-- https://heroicons.dev/ - phone - MIT Licence  -->
+    <svg
+      class="w-6 h-6"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+      ></path>
+    </svg>
+    <a :href="`tel:${telefonnumber}`">
+      <span class="ml-4">{{ telefonnumber }}</span>
+    </a>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    telefonnumber: { type: String, required: true },
+  },
+}
+</script>
