447b137c97f8f593e8cdfe69905501fa76f5afc2
Daniel Naschberger <daniel@naschi.at>
2021-06-12 10:46:39 +0200

    fix(Buttons): margin

--- components/ButtonCalendar.vue
+++ components/ButtonCalendar.vue
@@ -5,6 +5,8 @@
       hover:bg-green
       text-green-darkest
       font-bold
+      my-2
+      mr-2
       py-2
       px-4
       rounded
--- components/ButtonContact.vue
+++ components/ButtonContact.vue
@@ -5,6 +5,10 @@
       hover:bg-green
       text-green-darkest
       font-bold
+      my-2
+      mr-2
+      my-2
+      mr-2
       py-2
       px-4
       rounded
