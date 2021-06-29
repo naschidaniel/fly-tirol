c9abbb921a04285ad88709b4edc2307f8c2b3389
Daniel Naschberger <daniel@naschi.at>
2021-06-29 19:23:46 +0200

    chore(ResponsiveImage): select imageSize

--- store/index.js
+++ store/index.js
@@ -3,13 +3,9 @@ import media from '~/static/media.json'
 export const state = () => ({
   isOpen: false,
   media,
-  windowWidth: 'lg',
 })
 
 export const mutations = {
-  setWindowWidth(state, change) {
-    state.windowWidth = change
-  },
   toggleDropdown(state) {
     state.isOpen = !state.isOpen
   },
