075e08e4e97da378ab9b8123655147c0b7065b94
Daniel Naschberger <daniel@naschi.at>
2021-06-29 20:14:17 +0200

    fix(ResponsiveImage): rm width and height props

--- components/ContentImage.vue
+++ components/ContentImage.vue
@@ -1,9 +1,7 @@
 <template>
   <responsive-image
     class="mb-2 object-contain rounded-lg mx-auto"
-    :hight="hight"
     :picture="picture"
-    :width="width"
   />
 </template>
 
@@ -12,9 +10,7 @@ import ResponsiveImage from './ResponsiveImage.vue'
 export default {
   components: { ResponsiveImage },
   props: {
-    hight: { type: String, default: 'h-100' },
     picture: { type: String, default: '', required: true },
-    width: { type: String, default: 'w-100' },
   },
 }
 </script>
