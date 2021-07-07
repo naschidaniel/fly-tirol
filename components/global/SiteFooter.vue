f8a1bd88c9fc24a1a226cdef60cde018125d8489
Daniel Naschberger <daniel@naschi.at>
2021-07-07 23:24:54 +0200

    chore(SiteFooter): add email and tel

--- components/global/SiteFooter.vue
+++ components/global/SiteFooter.vue
@@ -6,6 +6,10 @@
           <span class="mr-6"> &#169; fly-tirol.com </span>
         </div>
         <div>
+          <a href="mailto:info@fly-tirol.com" class="underline ml-6"
+            >info@fly-tirol.com</a
+          >
+          <a href="tel:00436766422095" class="underline ml-6">+43 676 642209</a>
           <nuxt-link to="/dsgvo" class="underline">
             <span class="ml-6">DSGVO</span>
           </nuxt-link>
