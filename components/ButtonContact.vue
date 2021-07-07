078d493f9b93b19f427186bc2a0642bc4b176a6a
Daniel Naschberger <daniel@naschi.at>
2021-07-07 22:38:51 +0200

    chore(components): rm unused components

--- components/ButtonContact.vue
+++ components/ButtonContact.vue
@@ -1,24 +1,6 @@
 <template>
-  <button
-    class="
-      bg-green-500
-      hover:bg-green
-      text-green-darkest
-      font-bold
-      my-2
-      mr-2
-      my-2
-      mr-2
-      py-2
-      px-4
-      rounded
-      inline-flex
-      items-center
-    "
-  >
+  <button class="btn-primary btn--large mt-4">
     <outline-chat-icon class="w-6 h-6" />
-    <a :href="`tel:${telefonnumber}`">
-      <nuxt-link to="/kontakt" class="ml-4">Buchen</nuxt-link>
-    </a>
+    <nuxt-link to="/kontakt" class="ml-4">Buchen</nuxt-link>
   </button>
 </template>
