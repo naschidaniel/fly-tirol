a6ea6c767b8b228cf0c705ce461362c0b8dcff41
Daniel Naschberger <daniel@naschi.at>
2021-07-07 23:18:13 +0200

    fix(InfoBoxTwo): simplify headlines

--- components/InfoBoxTwo.vue
+++ components/InfoBoxTwo.vue
@@ -2,10 +2,10 @@
   <div class="max-w-90 mx-auto text-lg font-heading font-medium leading-snug">
     <div class="h-0.75 bg-brand rounded-full w-full"></div>
     <div class="flex justify-center py-8 px-8">
-      <outline-location-marker-icon class="w-5 h-5 mr-1 -mt-0.25" /> Anschrift
-      <h1 class="mr-6">
-        {{ boxTitle }}
-      </h1>
+      <outline-location-marker-icon class="w-5 h-5 mr-1 -mt-0.25" />
+      <span class="mr-6">
+        {{ headline }}
+      </span>
       {{ address }}
     </div>
     <div class="h-0.75 bg-brand rounded-full w-full"></div>
@@ -16,7 +16,7 @@
 export default {
   props: {
     address: { type: String, required: true },
-    boxTitle: { type: String, required: true },
+    headline: { type: String, required: true },
   },
 }
 </script>
