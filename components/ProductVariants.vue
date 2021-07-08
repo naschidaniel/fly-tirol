057af7a85d41242e01ce3af821afb6b40bc4b9eb
Daniel Naschberger <daniel@naschi.at>
2021-07-08 16:30:15 +0200

    fix(shopify): display dates

--- components/ProductVariants.vue
+++ components/ProductVariants.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="m-4">
-    Termine
+  <div class="m-4 my-auto">
+    <h2 class="font-bold">Termine</h2>
     <ul>
       <li v-for="variant in productVariants" :key="variant.id">
         {{ variant.title }}
       </li>
     </ul>
+    <button-contact />
   </div>
 </template>
 
 <script>
+import ButtonContact from '~/components/ButtonContact.vue'
+
 export default {
+  components: { ButtonContact },
   computed: {
     productVariants() {
       const products = this.$store.state.products.filter(
