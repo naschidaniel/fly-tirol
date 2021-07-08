a86d22af9036ee3b0761a28b20eb8b245c74ec22
Daniel Naschberger <daniel@naschi.at>
2021-07-08 16:30:15 +0200

    fix(shopify): display dates

--- pages/ausbildung/_slug.vue
+++ pages/ausbildung/_slug.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="max-w-90 mx-auto pt-8">
     <nuxt-content :document="page" />
-    <button-contact />
     <product-variants />
   </div>
 </template>
 
 <script>
-import ButtonContact from '~/components/ButtonContact.vue'
 import ProductVariants from '~/components/ProductVariants.vue'
 import { generateMetatags } from '~/util/generateHeaderInformation'
 
 export default {
-  components: { ButtonContact, ProductVariants },
+  components: { ProductVariants },
   async asyncData({ $content, params }) {
     const page = await $content('ausbildung', params.slug).fetch()
     return { page }
