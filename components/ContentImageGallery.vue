b43287bd2cac6213b3d9c3d9378b1cdef809ec77
Daniel Naschberger <daniel@naschi.at>
2021-07-08 16:13:39 +0200

    chore(ContentImageGallery): refactoring styling

--- components/ContentImageGallery.vue
+++ components/ContentImageGallery.vue
@@ -1,18 +1,24 @@
 <template>
-  <section class="py-8 w-100">
-    <div class="flex flex-wrap">
-      <div v-for="img in imageGalery" :key="img.url" class="w-1/2 md:w-1/4 p-2">
-        <div class="aspect-w-1 aspect-h-1 overflow-hidden">
-          <responsive-image
-            img-class="rounded shadow-md object-cover w-full h-full"
-            :alt="img.alt"
-            :picture="img.url"
-            :title="img.title"
-          />
+  <div class="w-full md:max-w-90 mx-auto">
+    <section class="py-8 w-100">
+      <div class="flex flex-wrap">
+        <div
+          v-for="img in imageGalery"
+          :key="img.url"
+          class="w-1/2 md:w-1/4 p-2"
+        >
+          <div class="aspect-w-1 aspect-h-1 overflow-hidden">
+            <responsive-image
+              img-class="rounded shadow-md object-cover w-full h-full"
+              :alt="img.alt"
+              :picture="img.url"
+              :title="img.title"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
