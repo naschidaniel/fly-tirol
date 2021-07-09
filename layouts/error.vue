9e162600a001e0c2bbe4e931c2e364883ee15482
Daniel Naschberger <daniel@naschi.at>
2021-07-09 16:13:16 +0200

    fix(error): set Object type

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,13 +2,25 @@
   <div>
     <h1 v-if="error.statusCode === 404">404 Page not found</h1>
     <h1 v-else>Es ist ein Fehler aufgetretten</h1>
-    <NuxtLink to="/">zurück zur Startseite</NuxtLink>
+    <nuxt-link to="/">zurück zur Startseite</nuxt-link>
   </div>
 </template>
 
 <script>
 export default {
   layout: 'error',
-  props: { error: { type: Number, default: 404 } },
+  props: {
+    error: {
+      type: Object,
+      default: () => {
+        return {
+          error: {
+            statusCode: 404,
+          },
+        }
+      },
+      required: false,
+    },
+  },
 }
 </script>
