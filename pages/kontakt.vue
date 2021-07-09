5eaf94cecd0d15bbaec590d3d8a0754bea2716b2
Daniel Naschberger <daniel@naschi.at>
2021-07-09 16:08:57 +0200

    chor(kontakt): init page

---
+++ pages/kontakt.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nuxt-content :document="page" />
+  </div>
+</template>
+
+<script>
+import { generateMetatags } from '~/util/generateHeaderInformation'
+
+export default {
+  components: {},
+  async asyncData({ $content }) {
+    const page = await $content('kontakt').fetch()
+    return { page }
+  },
+  data() {
+    return {}
+  },
+  head() {
+    const metatags = generateMetatags(this.page.title, this.page.description)
+    return { title: this.page.title, meta: metatags }
+  },
+}
+</script>
