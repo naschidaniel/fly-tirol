b49fd8439e3790feefae723b6d95aea364aae478
Daniel Naschberger <daniel@naschi.at>
2021-07-09 17:10:19 +0200

    chore(TandemFlightList): drop swiper

--- components/TandemFlightList.vue
+++ components/TandemFlightList.vue
@@ -1,52 +1,23 @@
 <template>
-  <div class="overflow-hidden pt-12">
-    <div class="w-full max-w-90 mx-auto">
-      <swiper class="swiper" :options="swiperOption">
-        <swiper-slide
-          v-for="flight in tandemflights"
-          :key="flight.title"
-          class="card--slide"
-        >
-          <tandem-flight-card :flight="flight" />
-        </swiper-slide>
-      </swiper>
+  <div class="max-w-90 mx-auto md:py-4 md:py-12">
+    <div class="flex flex-wrap justify-center">
+      <div
+        v-for="flight in tandemflights"
+        :key="flight.title"
+        class="m-1 md:m-3 w-80"
+      >
+        <tandem-flight-card :flight="flight" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
 import TandemFlightCard from './TandemFlightCard.vue'
 export default {
-  components: { TandemFlightCard, Swiper, SwiperSlide },
+  components: { TandemFlightCard },
   props: { tandemflights: { type: Array, required: true } },
-  data() {
-    return {
-      swiperOption: {
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        initialSlide: 0,
-        watchOverflow: true,
-        spaceBetween: 12,
-        grabCursor: true,
-        keyboard: {
-          enabled: true,
-        },
-        slideClass: 'card--slide',
-        breakpoints: {
-          640: {
-            spaceBetween: 32,
-            initialSlide: 1,
-          },
-        },
-        // navigation: {
-        //   nextEl: '.slider-button-next',
-        //   prevEl: '.slider-button-prev',
-        // },
-      },
-    }
-  },
 }
 </script>
 
