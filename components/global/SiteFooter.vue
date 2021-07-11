4117e10643f3b0525768c504bf6dc24ac9d27925
moelp <dev@moelp.com>
2021-07-11 16:40:51 +0200

    Update Footer - Add Contact Data

--- components/global/SiteFooter.vue
+++ components/global/SiteFooter.vue
@@ -1,23 +1,57 @@
 <template>
-  <div class="w-full pt-8">
-    <div class="bg-gray-200">
-      <div class="flex justify-between py-4 text-xs max-w-90 mx-auto">
-        <div>
-          <span class="mr-6"> &#169; fly-tirol.com </span>
-        </div>
-        <div>
-          <nuxt-link to="/dsgvo" class="underline">
-            <span class="ml-2 md:ml-6">DSGVO</span>
-          </nuxt-link>
-          <nuxt-link to="/impressum" class="underline">
-            <span class="ml-2 md:ml-6">Impressum</span>
-          </nuxt-link>
-          <a href="mailto:info@fly-tirol.com" class="underline"
-            ><span class="ml-2 md:ml-6">info@fly-tirol.com</span></a
-          >
-          <a href="tel:00436766422095" class="underline"
-            ><span class="ml-2 md:ml-6">+43 676 642209</span></a
-          >
+  <div class="pt-16">
+    <div class="w-full py-8 md:py-6 bg-gray-200">
+      <div class="max-w-90 w-full mx-auto text-md sm:text-xs">
+        <div class="flex flex-wrap md:flex-nowrap items-center justify-between">
+          <div class="relative flex-shrink-0 w-full md:w-auto mb-8 md:mb-0">
+            <nuxt-link
+              to="/"
+              class="block outline-none w-24 whitespace-nowrap"
+              exact
+            >
+              &#169; fly-tirol.com
+            </nuxt-link>
+          </div>
+          <div class="flex flex-wrap sm:flex-nowrap justify-between w-full">
+            <div class="flex flex-wrap w-full md:justify-center mb-8 sm:mb-0">
+              <a
+                href="mailto:info@fly-tirol.com"
+                class="underline whitespace-nowrap"
+              >
+                info@fly-tirol.com
+              </a>
+              <a
+                href="tel:00436766422095"
+                class="underline ml-8 sm:ml-4 md:ml-6 whitespace-nowrap"
+              >
+                +43 676 642209
+              </a>
+              <div class="mt-8 sm:mt-0 sm:ml-4 md:ml-6 w-full sm:w-auto">
+                <a
+                  href="https://www.instagram.com/fly.tirol/"
+                  target="_blank"
+                  class="
+                    inline-flex
+                    items-center
+                    leading-none
+                    whitespace-nowrap
+                    underline
+                  "
+                >
+                  <InstagramLogo class="ft-icon mr-2" />
+                  Folge uns auf Instagram
+                </a>
+              </div>
+            </div>
+            <div class="flex w-full sm:w-auto sm:ml-4">
+              <nuxt-link to="/dsgvo" class="underline">
+                <span>DSGVO</span>
+              </nuxt-link>
+              <nuxt-link to="/impressum" class="underline ml-8 sm:ml-4 md:ml-6">
+                <span>Impressum</span>
+              </nuxt-link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
