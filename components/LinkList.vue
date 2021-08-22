dbeaee2e075f41f6669e9e24dd0737fd55ae1604
Daniel Naschberger <daniel@naschi.at>
2021-08-22 12:03:12 +0200

    chore(index): replace swiper

--- components/LinkList.vue
+++ components/LinkList.vue
@@ -1,162 +1,101 @@
 <template>
-  <div class="overflow-hidden">
-    <div class="max-w-90 mx-auto lg:max-w-none lg:w-10/12 lg:mr-0 lg:ml-auto">
-      <div class="hidden lg:flex justify-end pr-10p mb-8 mt-8">
-        <button
-          class="
-            inline-flex
-            flex-grow-0 flex-initial
-            border-2 border-brand
-            rounded-full
-            mr-4
-            slider-button-prev
-            focus:ring-2
-            focus:ring-blue-400
-            focus:ring-offset-2
-            focus:ring-offset-white
-            focus:ring-opacity-50
-            focus:outline-none
-          "
-        >
-          <solid-chevron-left-icon class="w-8 h-8" />
-        </button>
-        <button
-          class="
-            inline-flex
-            flex-grow-0 flex-initial
-            border-2 border-brand
-            rounded-full
-            slider-button-next
-            focus:ring-2
-            focus:ring-blue-400
-            focus:ring-offset-2
-            focus:ring-offset-white
-            focus:ring-opacity-50
-            focus:outline-none
-          "
-        >
-          <solid-chevron-right-icon class="w-8 h-8" />
-        </button>
+  <div class="max-w-90 mx-auto 2xl:mr-6 2xl:ml-auto">
+    <div class="flex flex-wrap 2xl:flex-nowrap">
+      <div class="flex flex-col w-full 2xl:w-96 max-w-screen-sm flex-shrink-0">
+        <div class="mt-6">
+          <h1 class="mb-6">
+            <span
+              class="
+                text-lg
+                font-heading font-medium
+                flex flex-row
+                items-center
+                mb-2
+              "
+            >
+              <span class="inline-block bg-brand w-7 h-0.75 mr-2"></span>
+              <span class="inline-block"
+                >Finde das richtige<picture>
+                  <source media="(min-width: )" srcset="" />
+                  <img src="" alt="" /> </picture
+              ></span>
+            </span>
+            <span class="text-5xl font-heading font-bold">Angebot</span>
+          </h1>
+          <p class="text-lg leading-relaxed max-w-prose">
+            Egal ob du einmal das Gefühl der Freiheit bei einem Tandemflug in
+            den wunderschönen Kitzbüheler Alpen genießen möchtest, du selbst ein
+            Paragleitpilot werden möchtest, deine vorhandenen Flugfähigkeiten
+            ausbauen willst oder mit uns auf Flugreise gehen möchtest - wir
+            haben genau das richtige für dich!
+          </p>
+        </div>
       </div>
-      <div class="flex flex-wrap lg:flex-nowrap">
-        <div class="flex flex-col w-full lg:w-96 max-w-screen-sm flex-shrink-0">
-          <div class="mt-6">
-            <h1 class="mb-6">
-              <span
-                class="
-                  text-lg
-                  font-heading font-medium
-                  flex flex-row
-                  items-center
-                  mb-2
-                "
-              >
-                <span class="inline-block bg-brand w-7 h-0.75 mr-2"></span>
-                <span class="inline-block">{{ title.pre }}</span>
-              </span>
-              <span class="text-5xl font-heading font-bold">
-                {{ title.main }}
-              </span>
-            </h1>
-            <p class="text-lg leading-relaxed max-w-prose">
-              {{ intro }}
-            </p>
-          </div>
-        </div>
-        <div class="w-full mt-8 lg:mt-0 lg:w-4/6 lg:pr-10p lg:ml-16">
-          <swiper class="swiper" :options="swiperOption">
-            <swiper-slide>
-              <link-card
-                :card-image="tandemflights.cardImage"
-                :card-title="tandemflights.cardTitle"
-                :description="tandemflights.description"
-                :link-name="tandemflights.linkName"
-                :to="tandemflights.to"
-              />
-            </swiper-slide>
-            <swiper-slide>
-              <link-card
-                :card-image="basicTrainings.cardImage"
-                :card-title="basicTrainings.cardTitle"
-                :description="basicTrainings.description"
-                :link-name="basicTrainings.linkName"
-                :to="basicTrainings.to"
-              />
-            </swiper-slide>
-            <swiper-slide>
-              <link-card
-                :card-image="advancedTrainings.cardImage"
-                :card-title="advancedTrainings.cardTitle"
-                :description="advancedTrainings.description"
-                :link-name="advancedTrainings.linkName"
-                :to="advancedTrainings.to"
-              />
-            </swiper-slide>
-            <swiper-slide>
-              <link-card
-                :card-image="saftyTrainings.cardImage"
-                :card-title="saftyTrainings.cardTitle"
-                :description="saftyTrainings.description"
-                :link-name="saftyTrainings.linkName"
-                :to="saftyTrainings.to"
-              />
-            </swiper-slide>
-          </swiper>
-        </div>
+      <div
+        class="
+          card--grid
+          grid-gap-1
+          max-w-90
+          mx-auto
+          py-4
+          w-full
+          xl:py-12 xl:ml-4
+          2xl:flex
+        "
+      >
+        <link-card
+          :card-image="tandemflights.cardImage"
+          :card-title="tandemflights.cardTitle"
+          :description="tandemflights.description"
+          :link-name="tandemflights.linkName"
+          :to="tandemflights.to"
+        />
+        <link-card
+          :card-image="basicTrainings.cardImage"
+          :card-title="basicTrainings.cardTitle"
+          :description="basicTrainings.description"
+          :link-name="basicTrainings.linkName"
+          :to="basicTrainings.to"
+        />
+        <link-card
+          :card-image="advancedTrainings.cardImage"
+          :card-title="advancedTrainings.cardTitle"
+          :description="advancedTrainings.description"
+          :link-name="advancedTrainings.linkName"
+          :to="advancedTrainings.to"
+        />
+        <link-card
+          :card-image="saftyTrainings.cardImage"
+          :card-title="saftyTrainings.cardTitle"
+          :description="saftyTrainings.description"
+          :link-name="saftyTrainings.linkName"
+          :to="saftyTrainings.to"
+        />
+        <link-card
+          :card-image="travels.cardImage"
+          :card-title="travels.cardTitle"
+          :description="travels.description"
+          :link-name="travels.linkName"
+          :to="travels.to"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import 'swiper/swiper-bundle.css'
 import LinkCard from './LinkCard.vue'
-import SolidChevronLeftIcon from './icons/SolidChevronLeftIcon.vue'
-import SolidChevronRightIcon from './icons/SolidChevronRightIcon.vue'
 
 export default {
   components: {
     LinkCard,
-    SolidChevronLeftIcon,
-    SolidChevronRightIcon,
-    Swiper,
-    SwiperSlide,
   },
   props: {
     advancedTrainings: { type: Object, default: () => {} },
     basicTrainings: { type: Object, required: true },
     saftyTrainings: { type: Object, required: true },
     tandemflights: { type: Object, required: true },
-    travel: { type: Object, default: () => {} },
-  },
-  data() {
-    return {
-      title: {
-        pre: 'Finde das richtige',
-        main: 'Angebot',
-      },
-      intro:
-        'Egal ob du einmal das Gefühl der Freiheit bei einem Tandemflug in den wunderschönen Kitzbüheler Alpen genießen möchtest, du selbst ein Paragleitpilot werden möchtest, deine vorhandenen Flugfähigkeiten ausbauen willst oder mit uns auf Flugreise gehen möchtest - wir haben genau das richtige für dich!',
-      swiperOption: {
-        slidesPerView: 'auto',
-        spaceBetween: 8,
-        grabCursor: true,
-        keyboard: {
-          enabled: true,
-        },
-        slideClass: 'card--slide',
-        navigation: {
-          nextEl: '.slider-button-next',
-          prevEl: '.slider-button-prev',
-        },
-        breakpoints: {
-          640: {
-            spaceBetween: 32,
-          },
-        },
-      },
-    }
+    travels: { type: Object, default: () => {} },
   },
 }
 </script>
