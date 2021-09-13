f5f27edf6fec8371633e5122a93793b0a277ea9b
Daniel Naschberger <daniel@naschi.at>
2021-09-13 20:39:15 +0200

    chore(ProductCard): add a spinner for shopify

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -20,19 +20,21 @@
             </span>
           </div>
           <div class="flex items-center text-sm -ml-1 mb-2">
-            <outline-cash-icon class="w-4 h-4" />
-            <span class="block leading-none pt-1 ml-1 font-bold">
+            <outline-cash-icon class="w-4 h-4 mr-1" />
+            <span v-if="price.price" class="block leading-none pt-1 font-bold">
               {{ price.preText }} {{ price.price | formatPrice }}
             </span>
+            <SpinnerIcon v-else class="animate-spin h-4 w-4 text-brand" />
           </div>
           <div
             v-if="!isTandemflight"
             class="flex items-center text-sm -ml-1 mb-4"
           >
-            <outline-calendar-icon class="w-4 h-4" />
-            <span class="block leading-none pt-1 ml-1"
+            <outline-calendar-icon class="w-4 h-4 mr-1" />
+            <span v-if="dates" class="block leading-none pt-1"
               >{{ dates }} Termine</span
             >
+            <SpinnerIcon v-else class="animate-spin h-4 w-4 text-brand" />
           </div>
           <p v-if="isTandemflight" class="text-gray-600">
             {{ page.description }}
@@ -59,6 +61,7 @@ import OutlineCashIcon from './icons/OutlineCashIcon.vue'
 import OutlineCalendarIcon from './icons/OutlineCalendarIcon.vue'
 import OutlineLocationMarkerIcon from './icons/OutlineLocationMarkerIcon.vue'
 import ResponsiveImage from './ResponsiveImage.vue'
+import SpinnerIcon from './icons/SpinnerIcon.vue'
 
 export default {
   components: {
@@ -66,6 +69,7 @@ export default {
     OutlineCalendarIcon,
     OutlineLocationMarkerIcon,
     ResponsiveImage,
+    SpinnerIcon,
   },
   props: {
     course: { type: Object, default: () => {} },
@@ -82,7 +86,7 @@ export default {
       return { preText, price: uniquePrices[0] }
     },
     dates() {
-      return this.course?.variants?.length || '-'
+      return this.course?.variants?.length
     },
   },
 }
