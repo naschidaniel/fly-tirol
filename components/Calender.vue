b23aa8f7e46fcf80f96d6102541e7c44dc1ab102
Daniel Naschberger <daniel@naschi.at>
2021-11-11 21:53:35 +0100

    chore(Calender): list available Products

--- components/Calender.vue
+++ components/Calender.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>Kalender</div>
+  <div>
+    Kalender
+    <div v-for="entry in calender" :key="entry.id">
+      {{ entry.title }} {{ entry.dateString }}
+      <span @click.prevent="bookProduct(entry.id)">Buchen</span>
+    </div>
+  </div>
 </template>
 
 <script>
 import { defineComponent } from '@vue/composition-api'
+import { useShop } from '~/composable/useShop'
 
 export default defineComponent({
   name: 'Calender',
+  setup() {
+    const { bookProduct, calender } = useShop()
+    return { bookProduct, calender }
+  },
 })
 </script>
