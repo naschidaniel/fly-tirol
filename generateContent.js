3f7702ab6af3fc2a7a012e1bd4a2f291931ee494
Daniel Naschberger <daniel@naschi.at>
2022-01-27 20:06:57 +0100

    chore(generateContent): parse markdown

---
+++ generateContent.js
@@ -0,0 +1,67 @@
+const fs = require('fs')
+const glob = require('glob')
+const marked = require('marked')
+
+const metadataJson = './static/metadata.json'
+
+let dataMetaDataJson = {}
+if (fs.existsSync(metadataJson)) {
+  dataMetaDataJson = JSON.parse(fs.readFileSync(metadataJson))
+}
+
+const content = glob.sync('./content/**/*.md').sort()
+
+dataMetaDataJson = {}
+content.forEach((filePath) => {
+  const category = filePath.split('/')[filePath.split('/').length - 2]
+  const handle = filePath
+    .split('/')
+    [filePath.split('/').length - 1].replace('.md', '')
+  const url = `/${category}/${handle}`
+
+  // parse markdown files
+  const data = fs.readFileSync(filePath, 'utf8')
+  const isContentImageGallery = data.includes('<ContentImageGallery ')
+  let metadata = { url, category, handle }
+  data
+    .split('---')[1]
+    .split(/\r\n|\n/)
+    .forEach((e) => {
+      const entry = e.trim().split(': ')
+      if (e.length === 0) {
+        return
+      }
+      metadata[entry[0]] = entry[1]
+    })
+  metadata = Object.keys(metadata)
+    .sort()
+    // eslint-disable-next-line no-sequences
+    .reduce((r, k) => ((r[k] = metadata[k]), r), {})
+  dataMetaDataJson[url] = metadata
+  const content = ['<template>', '<div>']
+  content.push(marked.marked(data.split('---')[2]).split(/\r\n|\n/))
+  content.push(['</div>', '</template>'])
+
+  if (isContentImageGallery) {
+    content.push([
+      '',
+      '<script>',
+      "import { defineComponent } from '@vue/composition-api'",
+      "import ContentImageGallery from '~/components/ContentImageGallery.vue'",
+      'export default defineComponent({',
+      '  components: { ContentImageGallery },',
+      '})',
+      '</script>',
+      '',
+    ])
+  }
+  const text = content.flat().join('\r\n')
+  fs.writeFile(filePath.replace('.md', '.vue'), text, (err) => {
+    if (err) throw err
+  })
+})
+
+const json = JSON.stringify(dataMetaDataJson, null, 2)
+fs.writeFile(metadataJson, json, (err) => {
+  if (err) throw err
+})
