4ab3d7b7d9bd8dd234497c51bea3cd809c5f3bc8
Daniel Naschberger <daniel@naschi.at>
2022-03-26 20:03:32 +0100

    nuxt.config: prettier

--- nuxt.config.js
+++ nuxt.config.js
@@ -154,7 +154,7 @@ export default {
   },
 
   server: {
-    port: process.env.NUXT_PAGE === 'whitecloud' ? 3001 : 3000 
+    port: process.env.NUXT_PAGE === 'whitecloud' ? 3001 : 3000,
   },
 
   babel: {
