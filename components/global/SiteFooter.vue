e7df2baa63127bb20fac772d890fdd11570794fc
Daniel Naschberger <daniel@naschi.at>
2022-03-28 21:17:12 +0200

    SiteFooter: add whitecloud

--- components/global/SiteFooter.vue
+++ components/global/SiteFooter.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="pt-16">
-    <div class="w-full py-8 md:py-6 bg-gray-200">
+    <div
+      class="w-full py-8 md:py-6"
+      :class="isFlyTirol ? 'bg-gray-200' : 'bg-[#333333] text-gray-300'"
+    >
       <div class="max-w-90 w-full mx-auto text-md sm:text-xs">
         <div class="flex flex-wrap md:flex-nowrap items-center justify-between">
           <div class="relative shrink-0 w-full md:w-auto mb-8 md:mb-0">
@@ -23,11 +26,25 @@
                 info@fly-tirol.com
               </a>
               <a
+                v-if="isWhiteCloud"
+                href="mailto:info@white-cloud.tirol"
+                class="underline whitespace-nowrap"
+              >
+                info@white-cloud.tirol
+              </a>
+              <a
                 v-if="isFlyTirol"
                 href="tel:00436766422088"
                 class="underline ml-8 sm:ml-4 md:ml-6 whitespace-nowrap"
               >
                 +43 676 6422088
+              </a>
+              <a
+                v-if="isWhiteCloud"
+                href="tel:004368181589568"
+                class="underline ml-8 sm:ml-4 md:ml-6 whitespace-nowrap"
+              >
+                +43 681 81589568
               </a>
               <span
                 v-if="isFlyTirol"
