b290be3ff8b7d855311e13b88c470e1702034f18
Daniel Naschberger <daniel@naschi.at>
2022-04-03 18:23:27 +0200

    icons: cloud and clock icons

--- components/icons/OutlineClockIcon.vue
+++ components/icons/OutlineClockIcon.vue
@@ -1,15 +1,17 @@
-<!-- https://heroicons.com/ - by @steveschoger - MIT Licensed - outline cloud icon -->
+// https://heroicons.com/ - by @steveschoger - MIT Licensed - outline cash icon
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
+    stroke-width="2"
   >
     <path
       stroke-linecap="round"
       stroke-linejoin="round"
-      d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z"
+      d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
     />
   </svg>
 </template>
