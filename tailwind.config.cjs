9adb6ad70657aae623807ede2799bfd6e1819f4a
Daniel Naschberger <daniel@naschi.at>
2022-07-10 19:33:25 +0200

    tailwind.config: direct set colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
 const isWhiteCloud = process.env.NUXT_PAGE === 'whitecloud'
 
-const colors = require('tailwindcss/colors')
 const em = (px) => `${px / 16}em`
 
 const screensizes = {
@@ -14,6 +13,58 @@ const screensizes = {
   // => @media (min-width: 1280px) { ... }
   '2xl': em(1536),
   // => @media (min-width: 1536px) { ... }
+}
+
+const colors = {
+  current: 'currentColor',
+  neutral: {
+    50: '#fafafa',
+    100: '#f5f5f5',
+    200: '#e5e5e5',
+    300: '#d4d4d4',
+    400: '#a3a3a3',
+    500: '#737373',
+    600: '#525252',
+    700: '#404040',
+    800: '#262626',
+    900: '#171717',
+  },
+  emerald: {
+    50: '#ecfdf5',
+    100: '#d1fae5',
+    200: '#a7f3d0',
+    300: '#6ee7b7',
+    400: '#34d399',
+    500: '#10b981',
+    600: '#059669',
+    700: '#047857',
+    800: '#065f46',
+    900: '#064e3b',
+  },
+  amber: {
+    50: '#fffbeb',
+    100: '#fef3c7',
+    200: '#fde68a',
+    300: '#fcd34d',
+    400: '#fbbf24',
+    500: '#f59e0b',
+    600: '#d97706',
+    700: '#b45309',
+    800: '#92400e',
+    900: '#78350f',
+  },
+  violet: {
+    50: '#f5f3ff',
+    100: '#ede9fe',
+    200: '#ddd6fe',
+    300: '#c4b5fd',
+    400: '#a78bfa',
+    500: '#8b5cf6',
+    600: '#7c3aed',
+    700: '#6d28d9',
+    800: '#5b21b6',
+    900: '#4c1d95',
+  },
 }
 
 const content = [
