3391a4c5cb3ac3411ea86aa8d7ae4781b904acd4
Daniel Naschberger <daniel@naschi.at>
2022-07-23 22:01:48 +0200

    Calender: rm Clickable span

--- components/Calender.vue
+++ components/Calender.vue
@@ -59,7 +59,7 @@
           class="m-2 p-4 border-2 rounded-lg md:w-2/3 lg:w-1/3 xl:w-1/4 text-left"
         >
           <span
-            class="inline-flex items-center justify-center px-2 py-1 leading-none text-gray-900 rounded-full cursor-pointer"
+            class="inline-flex items-center justify-center px-2 py-1 leading-none text-gray-900 rounded-full"
             :class="
               entry.productType === 'Ausbildung'
                 ? 'bg-green-300'
@@ -71,7 +71,6 @@
                 ? 'bg-red-200'
                 : 'bg-gray-100'
             "
-            @click="setCheckedProducts(product)"
             >{{ entry.productType }}</span
           >
           <h4>
