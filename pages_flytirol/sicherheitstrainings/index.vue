d47aa4426c8de1b8925543abbf27cddf2da8183a
FlyTirol22 <143787316+FlyTirol22@users.noreply.github.com>
2023-11-07 19:19:09 +0100

    Update index.vue

--- pages_flytirol/sicherheitstrainings/index.vue
+++ pages_flytirol/sicherheitstrainings/index.vue
@@ -2,13 +2,15 @@
   <div>
     <PageHeader pre-headline="Paragliding" headline="Sicherheitstrainings">
       Bist du bereits ein Paragleitpilot mit einem A-Schein und suchst nach
-      einer optimalen Vorbereitung auf außergewöhnliche Flugsituationen? Dann
-      heißen wir dich herzlich willkommen in der renommierten Flugschule „Fly
+      einer optimalen Vorbereitung auf außergewöhnliche Flugsituationen? 
+      
+      Dann heißen wir dich herzlich willkommen in der renommierten Flugschule „Fly
       Tirol“. Unter der fachkundigen Anleitung von Sebastian Kahn, Team
       Weltmeister im Acro-Paragleiten 2014 und Synchron-Acrobatic-Paragleiten
       Vizeweltmeister, bieten wir dir die einzigartige Gelegenheit, an zwei
       atemberaubenden Standorten zu trainieren: am malerischen Lago Di Garda in
       Italien und in der faszinierenden Umgebung von Ölüdeniz in der Türkei.
+      
       Diese exklusive Erfahrung ermöglicht es dir, unter realistischen
       Bedingungen Extremsituationen zu simulieren und wichtige Techniken wie die
       Steilspirale und den B-Stall zu erlernen oder zu perfektionieren. Unser
@@ -16,12 +18,13 @@
       Flugsituationen in der Zukunft souverän zu meistern oder sogar zu
       vermeiden. Das Training in diesen atemberaubenden Umgebungen bietet nicht
       nur eine umfassende fachliche Ausbildung, sondern auch eine unvergessliche
-      Erfahrung in zwei der schönsten Fluggebiete Europas. Unsere Experten
-      stehen bereit, um dir umfassende Kenntnisse und praktische Fähigkeiten zu
-      vermitteln, damit du stets selbstbewusst und sicher in der Luft agieren
-      kannst. Wir laden dich ein, dich uns anzuschließen, um ein umfassendes
-      Training zu erleben, das deine Flugerfahrung auf ein neues Niveau heben
-      wird. Fliege mit uns, um dich auf jedes Flugszenario vorzubereiten und
+      Erfahrung in zwei der schönsten Fluggebiete Europas. 
+      
+      Unsere Experten stehen bereit, um dir umfassende Kenntnisse und praktische 
+      Fähigkeiten zu vermitteln, damit du stets selbstbewusst und sicher in der 
+      Luft agieren kannst. Wir laden dich ein, dich uns anzuschließen, um ein 
+      umfassendes Training zu erleben, das deine Flugerfahrung auf ein neues Niveau 
+      heben wird. Fliege mit uns, um dich auf jedes Flugszenario vorzubereiten und
       deine Fähigkeiten zu perfektionieren.
     </PageHeader>
     <ProductList />
