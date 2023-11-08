c73541db3930d89e2f562759b830bdb7222a1941
Daniel Naschberger <daniel@naschi.at>
2023-11-08 20:45:34 +0100

    chore: lint

--- pages_flytirol/sicherheitstrainings/index.vue
+++ pages_flytirol/sicherheitstrainings/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div>
     <PageHeader pre-headline="Paragliding" headline="Sicherheitstrainings">
-      Bist du bereits ein Paragleitpilot mit einem A-Schein und suchst nach einer optimalen Vorbereitung auf außergewöhnliche Flugsituationen? Unter der fachkundigen Anleitung von Sebastian Kahn, Team Weltmeister im Acro-Paragleiten 2014 und Synchron-Acrobatic-Paragleiten Vizeweltmeister, bieten wir dir die einzigartige Gelegenheit, an zwei atemberaubenden Standorten zu trainieren: am malerischen Lago Di Garda in Italien und in der faszinierenden Umgebung von Ölüdeniz in der Türkei. 
-      Diese exklusive Erfahrung ermöglicht es dir, unter realistischen Bedingungen Extremsituationen zu simulieren und wichtige Techniken wie die Steilspirale und den B-Stall zu erlernen oder zu perfektionieren. Unser Ziel ist es, dir das nötige Rüstzeug zu vermitteln, um kritische Flugsituationen in der Zukunft souverän zu meistern oder sogar zu vermeiden. 
-      Das Training in diesen atemberaubenden Umgebungen bietet nicht nur eine umfassende fachliche Ausbildung, sondern auch eine unvergessliche Erfahrung in zwei der schönsten Fluggebiete Europas. Unsere Experten stehen bereit, um dir umfassende Kenntnisse und praktische Fähigkeiten zu vermitteln, damit du stets selbstbewusst und sicher in der Luft agieren kannst.
+      Bist du bereits ein Paragleitpilot mit einem A-Schein und suchst nach
+      einer optimalen Vorbereitung auf außergewöhnliche Flugsituationen? Unter
+      der fachkundigen Anleitung von Sebastian Kahn, Team Weltmeister im
+      Acro-Paragleiten 2014 und Synchron-Acrobatic-Paragleiten Vizeweltmeister,
+      bieten wir dir die einzigartige Gelegenheit, an zwei atemberaubenden
+      Standorten zu trainieren: am malerischen Lago Di Garda in Italien und in
+      der faszinierenden Umgebung von Ölüdeniz in der Türkei. Diese exklusive
+      Erfahrung ermöglicht es dir, unter realistischen Bedingungen
+      Extremsituationen zu simulieren und wichtige Techniken wie die
+      Steilspirale und den B-Stall zu erlernen oder zu perfektionieren. Unser
+      Ziel ist es, dir das nötige Rüstzeug zu vermitteln, um kritische
+      Flugsituationen in der Zukunft souverän zu meistern oder sogar zu
+      vermeiden. Das Training in diesen atemberaubenden Umgebungen bietet nicht
+      nur eine umfassende fachliche Ausbildung, sondern auch eine unvergessliche
+      Erfahrung in zwei der schönsten Fluggebiete Europas. Unsere Experten
+      stehen bereit, um dir umfassende Kenntnisse und praktische Fähigkeiten zu
+      vermitteln, damit du stets selbstbewusst und sicher in der Luft agieren
+      kannst.
     </PageHeader>
     <ProductList />
     <SocialBar />
