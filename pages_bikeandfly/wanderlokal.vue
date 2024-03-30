35d0c30360d59ea4be748273720f55383f0aacde
Daniel Naschberger <daniel@naschi.at>
2024-03-30 08:45:44 +0100

    chore: add lorem ipsum to wanderlokal

--- pages_bikeandfly/wanderlokal.vue
+++ pages_bikeandfly/wanderlokal.vue
@@ -6,6 +6,10 @@
       picture="/media/page-header.jpg"
     >
       <p>Wir sind KochArt.</p>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam
+      </p>
     </PageHeader>
     <div class="max-w-90 w-full mx-auto py-6 nuxt-content">
       <Wanderlokal />
