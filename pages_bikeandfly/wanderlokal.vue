16e53afae8119972597c35dca77e654690aa2f2a
FlyTirol22 <143787316+FlyTirol22@users.noreply.github.com>
2024-03-30 10:27:39 +0100

    Update wanderlokal.vue

--- pages_bikeandfly/wanderlokal.vue
+++ pages_bikeandfly/wanderlokal.vue
@@ -5,11 +5,9 @@
       headline="Wanderlokal"
       picture="/media/page-header.jpg"
     >
+      <p>Wo unser Foodtruck haltmacht, verströmt er den Duft der großen, weiten Welt der Kulinarik.</p>
+      <p>Von exotischen Spezialitäten bis hin zu vertrauten Klassikern genießen Sie frische Köstlichkeiten und originelle Rezepte, die wir vor Ort im Truck zubereiten.</p>
       <p>Wir sind KochArt.</p>
-      <p>
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam
-      </p>
     </PageHeader>
     <div class="max-w-90 w-full mx-auto py-6 nuxt-content">
       <Wanderlokal />
