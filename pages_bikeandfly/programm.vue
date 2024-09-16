0a9622e2ef1cf9dc0d11e2cbb50d5a64045ec6b9
FlyTirol22 <143787316+FlyTirol22@users.noreply.github.com>
2024-09-16 11:32:36 +0200

    Update programm.vue

--- pages_bikeandfly/programm.vue
+++ pages_bikeandfly/programm.vue
@@ -6,8 +6,11 @@
       picture="/media/page-header.jpg"
     >
       <p>
-        Vom <strong>09.08. – 11.08.2024</strong> erwartet euch erstmalig ein
-        absolutes Highlight in Westendorf!
+        Die Vorbereitungen für 2025 laufen bereits auf Hochtouren! 
+        
+        Es erwartet euch wieder ein unvergessliches Erlebnis mit vielen Highlights und neuen Überraschungen, die das Event noch spektakulärer machen werden.
+        
+        Bleibt gespannt auf weitere Details!
       </p>
     </PageHeader>
     <div class="max-w-90 w-full mx-auto py-6 nuxt-content">
