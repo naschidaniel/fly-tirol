7220740eb9fbf3c565ae9599640738bec343c37a
FlyTirol22 <143787316+FlyTirol22@users.noreply.github.com>
2024-09-16 12:13:29 +0200

    Update index.vue

--- pages_bikeandfly/shop/index.vue
+++ pages_bikeandfly/shop/index.vue
@@ -2,21 +2,21 @@
   <div>
     <PageHeader
       pre-headline="Bike&Fly. Festival Brixental"
-      headline="Shop"
+      headline="Expo Area"
       picture="/media/page-header.jpg"
     >
       <div>
         <p class="mb-2">
-          Du möchtest unbedingt beim ersten Bike & Fly Festival dabei sein? Dann
-          sichere dir schon jetzt deine Tickets und spare beim Onlinekauf Geld.
-          Natürlich sind auch Kurzentschlossene herzlich willkommen und können
-          ihre Tickets über die Kassen vor Ort erhalten.
+          Besuche unsere Expo Area und tauche ein in die Welt der besten Bike- und Gleitschirmhersteller!
+          Hier hast du die Gelegenheit, die neuesten Modelle und innovativsten Ausstattungen aus erster Hand zu erleben.
+          Schlendere durch die Stände, lass dich von den Experten beraten, und teste die neuesten Bikes und Gleitschirme direkt vor Ort.
+          Finde genau das Equipment, das zu deinem Abenteuer passt, und genieße die Vielfalt an Marken und Produkten, die wir für dich bereithalten.
+          Verpasse nicht die Chance, dich vor Ort inspirieren zu lassen und deinen persönlichen Favoriten zu entdecken!
         </p>
         <p class="mb-2">
           An diesem Wochenende gibt es von Fly Tirol und Morgensonne auf alle
           Bike & Fly Angebot spezielle Vergünstigungen für alle
-          Festival-Besucher. Reserviere dir schon jetzt deinen Platz für ein
-          einzigartiges Erlebnis!
+          Festival-Besucher. Seid Bereit!
         </p>
       </div>
     </PageHeader>
