9c55608305783a5ddb5e3a9ccadc1c6dcab2a66d
Daniel Naschberger <daniel@naschi.at>
2024-09-23 20:35:11 +0200

    chore: upgrade list

--- pages_gh2di/index.vue
+++ pages_gh2di/index.vue
@@ -5,27 +5,30 @@
     <div
       class="relative max-w-90 mx-auto w-full lg:w-11/12 lg:mr-0 lg:ml-auto lg:py-24 z-10"
     >
-      <div class="flex flex-col max-w-screen-sm lg:shrink-0 -mt-24 md:mt-0">
+      <div class="flex flex-col max-w-screen-xl lg:shrink-0 -mt-24 md:mt-0">
         <div class="mt-12 sm:mt-6">
           <h1 class="mb-6">
             <span
               class="text-lg font-heading font-medium flex flex-row items-center mb-2"
             >
               <span class="inline-block bg-brand w-7 h-0.75 mr-2" />
-              <span class="inline-block text-white">Hydrogen Advisers</span>
+              <span class="inline-block text-white">GH2DI.eu</span>
             </span>
             <span
               class="text-4xl lg:text-6xl font-heading font-bold text-white"
             >
-              GH2DI.eu
+              Green Hydrogen Development & Investment GmbH
             </span>
           </h1>
           <p class="text-lg leading-relaxed max-w-prose text-white">
-            We are an advisory service to provide strategic advice to all
-            actors seeking to advance their green hydrogen Agenda and to start
-            roll-out.
+            We are working on research, development and investment in Technologies in the broad value chain of green hydrogen. Our current focus is:
           </p>
-          <p class="text-lg leading-relaxed max-w-prose text-white mt-4">
+          <ul>
+            <li>a) local seasonal hydrogen storage</li>
+            <li>b) a system for energy autonomy of buildings based on PV and with green hydrogen as seasonal energy storage</li>
+            <li>c) storage solutions for hydrogen mobility</li>
+          </ul>
+          <p class="text-lg leading-relaxed max-w-prose text-white mt-10">
             <strong>Thank you for your visit. We are currently working on our website and would be pleased if you visit us at a later date.</strong>
           </p>
           <p class="text-lg leading-relaxed max-w-prose text-white mt-4">
