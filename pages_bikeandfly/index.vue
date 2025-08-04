9b53db1634a70790ddba4a176952ac7472be4764
Daniel Naschberger <daniel@naschi.at>
2025-08-04 21:57:42 +0200

    chore: add urlito

--- pages_bikeandfly/index.vue
+++ pages_bikeandfly/index.vue
@@ -71,6 +71,18 @@
           allowfullscreen
         />
       </div>
+      <div class="flex justify-center items-center mb-6">
+        <iframe
+          width="1120"
+          height="630"
+          src="https://www.youtube.com/embed/_BNdR0etXnc?si=IjFdt5MLzxIQXMJd"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        />
+      </div>
     </div>
     <SocialBar />
   </div>
